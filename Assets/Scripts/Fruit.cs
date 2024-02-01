7e7cd043fa9ca267e6cc859b42996ccf32ee67f3
pijussJ <pijus.jarusevicius3@gmail.com>
2024-02-01 17:29:19 +0200

    Day 1 done

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -8,7 +8,7 @@ public class Fruit : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(0, 10);
+        rb.velocity = new Vector2(0, Random.Range(10,15));
         rb.angularVelocity = 250;
     }
     private void Update()
